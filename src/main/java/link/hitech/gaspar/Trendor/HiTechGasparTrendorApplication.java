044b2bfc4ca51c0188e2c63d3df0a363ecee08e8
nmpg <nuno@gaspar.link>
2020-11-15 15:27:51 +0100

    First version - generated by Spring Initializr

---
+++ src/main/java/link/hitech/gaspar/Trendor/HiTechGasparTrendorApplication.java
@@ -0,0 +1,13 @@
+package link.hitech.gaspar.Trendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiTechGasparTrendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiTechGasparTrendorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/link/hitech/gaspar/Trendor/HiTechGasparTrendorApplicationTests.java
@@ -0,0 +1,13 @@
+package link.hitech.gaspar.Trendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiTechGasparTrendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
